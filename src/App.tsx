922f537fd5c016da0e7ae83013244a7882ebf0c6
User <user@example.com>
2026-01-21 20:19:56 +0100

    Update Twitter link to new X.com URL in App component

--- src/App.tsx
+++ src/App.tsx
@@ -194,7 +194,7 @@ function App() {
         </div>
 
         <div className="social-links">
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="social-btn twitter">
+          <a href="https://x.com/MYCELIUM_dd" target="_blank" rel="noopener noreferrer" className="social-btn twitter">
             <svg viewBox="0 0 24 24" className="social-icon">
               <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
             </svg>
